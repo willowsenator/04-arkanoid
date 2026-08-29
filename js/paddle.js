0637b65c9b8ba1f618f3776c30cf6f9af6de122f
willowsenator <willowsenator@gmail.com>
2026-08-29 15:29:23 +0200

    Add paddle module with clamped movement

---
+++ js/paddle.js
@@ -0,0 +1,34 @@
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('./geometry.js'));
+  } else {
+    root.Arkanoid = root.Arkanoid || {};
+    root.Arkanoid.Paddle = factory(root.Arkanoid.Geometry);
+  }
+})(typeof self !== 'undefined' ? self : this, function (Geometry) {
+  function create(options) {
+    const paddle = {
+      x: options.x,
+      y: options.y,
+      width: options.width,
+      height: options.height,
+      canvasWidth: options.canvasWidth
+    };
+
+    paddle.setX = function (x) {
+      paddle.x = Geometry.clamp(x, 0, paddle.canvasWidth - paddle.width);
+    };
+
+    paddle.moveBy = function (dx) {
+      paddle.setX(paddle.x + dx);
+    };
+
+    paddle.getBounds = function () {
+      return { x: paddle.x, y: paddle.y, width: paddle.width, height: paddle.height };
+    };
+
+    return paddle;
+  }
+
+  return { create: create };
+});
---
+++ tests/paddle.test.js
@@ -0,0 +1,39 @@
+// tests/paddle.test.js
+const { test } = require('node:test');
+const assert = require('node:assert/strict');
+const Paddle = require('../js/paddle.js');
+
+function makePaddle() {
+  return Paddle.create({ x: 280, y: 450, width: 80, height: 10, canvasWidth: 640 });
+}
+
+test('moveBy shifts the paddle position', () => {
+  const paddle = makePaddle();
+  paddle.moveBy(10);
+  assert.equal(paddle.x, 290);
+});
+
+test('moveBy clamps at the left edge', () => {
+  const paddle = makePaddle();
+  paddle.moveBy(-500);
+  assert.equal(paddle.x, 0);
+});
+
+test('moveBy clamps at the right edge', () => {
+  const paddle = makePaddle();
+  paddle.moveBy(1000);
+  assert.equal(paddle.x, 640 - 80);
+});
+
+test('setX clamps out-of-range values', () => {
+  const paddle = makePaddle();
+  paddle.setX(-50);
+  assert.equal(paddle.x, 0);
+  paddle.setX(10000);
+  assert.equal(paddle.x, 640 - 80);
+});
+
+test('getBounds returns the current rect', () => {
+  const paddle = makePaddle();
+  assert.deepEqual(paddle.getBounds(), { x: 280, y: 450, width: 80, height: 10 });
+});
